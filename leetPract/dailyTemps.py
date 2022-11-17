fcbdcf0a5ae9cc5c6829c995a7d07c1d5e7e4bca
Alex Urmaza <aurmaza@albany.edu>
2022-11-17 00:49:54 -0500

    Update dailyTemps.py

--- leetPract/dailyTemps.py
+++ leetPract/dailyTemps.py
@@ -11,8 +11,10 @@ def dailyTemperatures(temperatures):
             answer[curr] = count - curr
         stack.append(count)
     return answer
-if __name__ =="__main__":
-    temps1=[73,74,75,71,69,72,76,73]
-    temps2=[30,40,50,60]
+
+
+if __name__ == "__main__":
+    temps1 = [73, 74, 75, 71, 69, 72, 76, 73]
+    temps2 = [30, 40, 50, 60]
     print(dailyTemperatures(temps1))
     print(dailyTemperatures(temps2))
